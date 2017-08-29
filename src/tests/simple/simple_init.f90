03186b9bf149e910e30609546523ae1195bb3fcb
Stefano Zaghi <stefano.zaghi@gmail.com>
2017-08-29 09:43:08 +0200

    clean code with minimal features enabled

--- src/tests/simple/simple_init.f90
+++ src/tests/simple/simple_init.f90
@@ -1,22 +1,16 @@
 !< FriVolous, Finite Volume block-structured Fortran test.
 !<
 !< A simple *use* and *init* test: it initializes a simple, Cartesian block.
+
 program simple_init
 !< FriVolous, Finite Volume block-structured Fortran test.
 !<
 !< A simple *use* and *init* test: it initializes a simple, Cartesian block.
-!-----------------------------------------------------------------------------------------------------------------------------------
-use frivolous
-!-----------------------------------------------------------------------------------------------------------------------------------
+use frivolous, only : block_object
 
-!-----------------------------------------------------------------------------------------------------------------------------------
 implicit none
-type(finite_volume_block) :: block
-!-----------------------------------------------------------------------------------------------------------------------------------
+type(block_object) :: ablock
 
-!-----------------------------------------------------------------------------------------------------------------------------------
-call block%init(cartesian=.true., Ni=3, Nj=4, Nk=5)
-print "(A, L1)", "Is the block Cartesian? ", block%is_cartesian()
-stop
-!-----------------------------------------------------------------------------------------------------------------------------------
+call ablock%initialize(cartesian=.true., Ni=3, Nj=4, Nk=5)
+print "(A, L1)", "Is the block Cartesian? ", ablock%is_cartesian()
 endprogram simple_init
